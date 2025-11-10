8799c220fd9834ec2abad5a9f02b3fcc5fe8a536
faristp <farismuhammed101@gmail.com>
2025-11-10 17:03:52 +0530

    mark done function / able to find the task to be done

--- todo.py
+++ todo.py
@@ -10,11 +10,28 @@ def list_task():
             print(f.read())
     except FileNotFoundError:
         print("No task Yet!")
-
+def mark_done(task):
+    try:
+        with open("task.txt","r") as f:
+            para = f.read().split('\n')
+            print(task)
+            for p in para:
+                
+                if task[0] in p:
+                    print(p, "to be done")
+                    p.replace("[ ]","[ Done ]")
+                else:
+                    print(p)
+                print("----")
+            
+    except FileNotFoundError:
+        print("No task to be done!")
 import sys
 if len(sys.argv) > 1:
     if sys.argv[1] == "add":
         add_task("".join(sys.argv[2:]))
     elif sys.argv[1]=="list":
         list_task()
+    elif sys.argv[1] == "mark":
+        mark_done(sys.argv[2:])
         
