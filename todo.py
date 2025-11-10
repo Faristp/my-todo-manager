8c9b0ebf1fb39ab6bce7cfb4ab8b2dfa7a89a766
Faristp <95973113+Faristp@users.noreply.github.com>
2025-11-10 16:37:40 +0530

    Merge pull request #1 from Faristp/add_task
    added a function to add task and list the tasks

--- todo.py
+++ todo.py
@@ -1,1 +1,20 @@
-print("my todo app v0.1")
+def add_task(task):
+    with open("task.txt", "a") as f:
+        f.write(f"[ ] {task} \n")
+    
+    print(f"Added task {task}")
+
+def list_task():
+    try:
+        with open("task.txt", "r") as f:
+            print(f.read())
+    except FileNotFoundError:
+        print("No task Yet!")
+
+import sys
+if len(sys.argv) > 1:
+    if sys.argv[1] == "add":
+        add_task("".join(sys.argv[2:]))
+    elif sys.argv[1]=="list":
+        list_task()
+        
