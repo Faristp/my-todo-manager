0e4aecedac5a474bdb68d0fae7a2cc96810c22c7
Faristp <95973113+Faristp@users.noreply.github.com>
2025-11-13 17:47:25 +0530

    Mark with ind (#2)
    * pass index to mark task as completed
    
    * small fixes

--- todo.py
+++ todo.py
@@ -1,13 +1,16 @@
 def add_task(task):
     with open("task.txt", "a") as f:
-        f.write(f"[ ] {task} \n")
+        f.writelines(f"[ ] {task} \n")
     
     print(f"Added task {task}")
 
 def list_task():
     try:
         with open("task.txt", "r") as f:
-            print(f.read())
+            
+            lines = f.readlines()
+            for i, line in enumerate(lines):
+                print(f"{i+1}. {line.strip()} ")
     except FileNotFoundError:
         print("No task Yet!")
 def mark_done(task):
@@ -16,17 +19,32 @@ def mark_done(task):
             lines = f.readlines()
             found = False
             task_text = " ".join(task)
-            for i,line in enumerate(lines):
-                if task_text in line and "[ ]" in line:
-                    lines[i] = line.replace("[ ]","[X]")
+            if task_text.isdigit():
+                i = int(task_text)
+                if i > len(lines) or i < 1:
+                    print("No task with the id ", i)
+                    return
+                line = lines[i-1]
+                lines[i-1]=lines[i-1].replace("[ ]","[X]")
+                with open("task.txt","w") as w:
+                    w.writelines(lines)
                     found = True
-                    break
+                
+            else:
+                for i,line in enumerate(lines):
+                    if task_text in line and "[ ]" in line:
+                        lines[i] = line.replace("[ ]","[X]")
+                        found = True
+                        with open("task.txt","w") as f:
+                            f.writelines(lines)
+                        break
             if not found:
                 print("task not found or already Completed")
             else:
-                with open("task.txt","w") as f:
-                    f.writelines(lines)
-        print(f"{task} marked done")
+                if task[0].isdigit():
+                    print(f"{lines[i-1][3:].strip()} marked done")
+                else:
+                    print(f"{" ".join(task)} marked done")
     except FileNotFoundError:
         print("No task to be done!")
 import sys
@@ -37,4 +55,5 @@ if len(sys.argv) > 1:
         list_task()
     elif sys.argv[1] == "mark":
         mark_done(sys.argv[2:])
-        
+else:
+    print("think before you tell") 
